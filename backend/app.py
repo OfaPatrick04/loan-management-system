aacaaeafb85583e6c4421f7fa14dc6366470b63b
Ofa Patrick <ofapatrick04@gmail.com>
2025-02-04 22:49:04 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "https://loan-management-system-frontend.vercel.app"}})
 
 # Loan evaluation logic
 def evaluate_loan(data):
